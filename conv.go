30024b470f5853d558dffba07f33968dd47a1657
515hikaru <karanodeny@gmail.com>
2018-01-03 23:48:14 +0900

    simple package created

--- conv.go
+++
@@ -1,7 +0,0 @@
-package tempconv
-
-// CToF は摂氏温度を華氏温度に変換する
-func CToF(c Celsius) Fahreheit { return Fahreheit(c*9/5 + 32) }
-
-// FToC は華氏温度を摂氏温度に変換する
-func FToC(f Fahreheit) Celsius { return Celsius((f - 32) * 5 / 9) }
